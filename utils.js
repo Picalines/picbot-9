d6924f358201b05aaa2ae932782e22479ec79289
Petr Kostenko <picalines0@gmail.com>
2020-08-26 03:46:09 +0300

    clamp util

--- utils.js
+++ utils.js
@@ -12,3 +12,10 @@ module.exports.randomRangeInt = (min, max) => {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+/**
+ * @param {number} value
+ * @param {number} min
+ * @param {number} max
+ */
+module.exports.clamp = (value, min, max) => Math.min(Math.max(value, min), max);
