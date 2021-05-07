2e1171e70af6b3124170cda60d5d28d7127af6e2
Petr Kostenko <picalines0@gmail.com>
2021-05-08 00:45:08 +0300

    mongoose db connected

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { Client } from 'discord.js';
 import { Bot } from 'picbot-engine';
+import { mongooseDatabaseHandler } from 'picbot-mongoose';
 
 import { prefixesState } from './states/prefixes.js';
 
@@ -8,8 +9,17 @@ const client = new Client();
 const bot = new Bot(client, {
     token: 'DISCORD_TOKEN',
     tokenType: 'env',
+
     fetchLocale: 'ru',
     fetchPrefixes: prefixesState,
+
+    databaseHandler: mongooseDatabaseHandler({
+        databaseUrl: 'mongodb://localhost/picbot-9',
+        connectOptions: {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    }),
 });
 
 bot.load();
